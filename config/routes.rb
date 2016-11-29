654a9b0f8077ac6f7b344d06d7f03e1a532dc0c4
Jerry Hoglen <jerryhoglen@me.com>
2016-11-29 18:42:26 -0500

    Create new controller for checkout page, create views for orders and create routes for orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   resources :products
+  
   root 'static_pages#landing_page'
+  
   get 'static_pages/index'
+  
   get 'static_pages/about'
+  
   get 'static_pages/contact'
+
+  resources :orders, only: [:index, :show, :create, :destroy]
 
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
