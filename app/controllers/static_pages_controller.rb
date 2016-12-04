f42163b969424a89b0cce7f1a22b27b5b0ae3362
Jerry Hoglen <jerryhoglen@me.com>
2016-12-04 13:29:49 -0500

    Trying to fix landing page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,7 @@
 class StaticPagesController < ApplicationController
-  def index
-  end
   
   def landing_page
-  	@products = Product.first 
-    @products = Product.limit(3)
+  	@products = Product.limit(3)
   end
 
 end
