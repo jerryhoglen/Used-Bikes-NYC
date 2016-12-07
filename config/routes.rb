faed878614a1e2af99762f280fe7a14098c115c6
Jerry Hoglen <jerryhoglen@me.com>
2016-12-07 08:57:21 -0500

    5.1 rerouted landing page and index page and used dynamic links

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   resources :products
   
-  root 'static_pages#landing_page'
+  root 'static_pages#index'
 
-  get 'static_pages/index'
+  get 'static_pages/landing_page'
   
   get 'static_pages/about'
   
