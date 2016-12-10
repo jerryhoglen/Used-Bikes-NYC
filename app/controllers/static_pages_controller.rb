22ad54f3ceaa880a87d25ccd1136f766ccd6dea4
Jerry Hoglen <jerryhoglen@me.com>
2016-12-10 09:49:46 -0500

    Sending Emails(Mailers) added sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,13 @@ class StaticPagesController < ApplicationController
   	@products = Product.limit(3)
   end
 
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email,
+  		:to => 'your-email@example.com',
+  		:subject => "A new contact for message from #{@name}",
+  		:body => @message).deliver_now
+  end
 end
