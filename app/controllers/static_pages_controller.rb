ebfca6eaec6e5d42e7c2cde8c8c3ea35c2f3202f
Jerry Hoglen <jerryhoglen@me.com>
2016-12-11 14:33:44 -0500

    5.3 update email

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,4 +9,5 @@ class StaticPagesController < ApplicationController
   	@email = params[:email]
   	@message = params[:message]
   	UserMailer.contact_form(@email, @name, @message).deliver_now
+  end
 end
