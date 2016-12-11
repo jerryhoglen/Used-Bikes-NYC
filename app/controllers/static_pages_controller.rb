2b82b30ba17e40316f002869b986cb572f437f02
Jerry Hoglen <jerryhoglen@me.com>
2016-12-11 14:22:19 -0500

    5.3 Sending Emails (Mailers)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,9 +8,5 @@ class StaticPagesController < ApplicationController
   	@name = params[:name]
   	@email = params[:email]
   	@message = params[:message]
-  	ActionMailer::Base.mail(:from => @email,
-  		:to => 'your-email@example.com',
-  		:subject => "A new contact for message from #{@name}",
-  		:body => @message).deliver_now
-  end
+  	UserMailer.contact_form(@email, @name, @message).deliver_now
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'jerryhoglen@me.com'
   layout 'mailer'
 end
