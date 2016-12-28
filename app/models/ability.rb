412f7266cc7f12a276c46dae16f88277404f93df
Jerry Hoglen <jerryhoglen@me.com>
2016-12-28 11:18:01 -0500

    updated abilities for users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,15 @@ class Ability
  def initialize(user)
     user ||= User.new # guest user (not logged in)
     can :manage, User, id: user.id
-  end
+    can :create, Comment
+    can :read, Comment
+    can :read, Product
 
-  
+    if user.admin?
+    	can :destroy, Comment
+    	can :manage, Product
+    end
+    #can :manage, Order, id: user.order.id
+  end
+end
 end
