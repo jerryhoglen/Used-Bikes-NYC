0fa037b7f57f2659616ffcfc34cf3e3e50469314
Jerry Hoglen <jerryhoglen@me.com>
2017-01-02 18:42:32 -0500

    small changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170102232406) do
+ActiveRecord::Schema.define(version: 20170102233105) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 20170102232406) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "colour"
+    t.integer  "price"
   end
 
   create_table "users", force: :cascade do |t|
