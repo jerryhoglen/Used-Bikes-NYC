b440c2bb4957bbd68c350fa6ee5af842274ae6fe
Jerry Hoglen <jerryhoglen@me.com>
2017-01-02 07:16:33 -0500

    Added Factory Girl

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,9 +1,21 @@
 FactoryGirl.define do
+	sequence(:email) { |n| "user#{n}@example.com}" }
+
 	factory :user do
-	email "jerryhoglen@me.com"
-    password "Maggie1!"
+	email 
+    password "Password1"
     first_name "Jerry"
     last_name "Hoglen"
     admin false
 	end
+
+	factory :admin, class: User do
+	email
+	password "Password1"
+	admin true
+	first_name "Admin"
+	last_name "User"
+	end
 end
+
+
