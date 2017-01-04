86a16b5d472441bc541f3f4f2fdf060978b363f9
Jerry Hoglen <jerryhoglen@me.com>
2017-01-03 21:19:06 -0500

    added sendgrid to app

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,9 +11,8 @@ class StaticPagesController < ApplicationController
   end
 
   def thank_you
-  	@name = params[:name]
-  	@email = params[:email]
-  	@message = params[:message]
-  	UserMailer.contact_form(@email, @name, @message).deliver_now
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
   end
-end
