7a5cd65987da6417889d8e43fd16b1c425e09198
Jerry Hoglen <jerryhoglen@me.com>
2017-01-09 13:38:55 -0500

    6.13 Real time updates with actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,10 @@ Rails.application.routes.draw do
 
   post 'static_pages/thank_you'
 
- devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout' } , :controllers => { :registrations => "user_registrations" } 
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout' } , :controllers => { :registrations => "user_registrations" } 
 
+  # actioncable
+  mount ActionCable.server => '/cable'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
