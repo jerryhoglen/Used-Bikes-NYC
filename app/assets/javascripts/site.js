a24d604bbed58614642a259f558e8e422fcc2dd5
Jerry Hoglen <jerryhoglen@me.com>
2017-01-08 20:19:25 -0500

    6.10 AJAX

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,11 +1,16 @@
- $(document).on('turbolinks:load', function(){
- 	  $(".img-zoom").elevateZoom();
+var refreshRating = function() {
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
     });
+};
+
+$(document).on('turbolinks:load ajaxSuccess', function() {
+    refreshRating();
+
+    $('.img-zoom').elevateZoom();
 });
 
