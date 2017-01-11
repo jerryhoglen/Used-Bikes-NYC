ed46953bf4170fda43ba838f5a19c108da8c73d7
Jerry Hoglen <jerryhoglen@me.com>
2017-01-10 23:45:01 -0500

    6.15 adjustments for achievement

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,18 +2,16 @@ require 'rails_helper'
 
 describe Product do 
 	
-
-
   before do 
     @product = Product.create!(name: "race bike", description: "fast race bike")
-	@user = User.create!(email: "jerryhoglen@me.com", password: "Maggie1!")
-	@product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
-	@product.comments.create!(rating: 3, user: @user, body: "Ok bike")
-	@product.comments.create!(rating: 5, user: @user, body: "Amazing bike!")
+		@user = User.create!(email: "jerryhoglen@me.com", password: "Maggie1!")
+		@product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+		@product.comments.create!(rating: 3, user: @user, body: "Ok bike")
+		@product.comments.create!(rating: 5, user: @user, body: "Amazing bike!")
   end
 
   it "returns the average rating of all comments" do
-	expect(@product.average_rating).to eq 3
+		expect(@product.average_rating).to eq 3
   end
 
   it "is not valid" do
